3c6e04f527ad695b0b5ec6810ec546727acb5af2
mlthelama <mlthelama@users.noreply.github.com>
2024-02-08 18:48:57 +0000

    maintain

--- src/setting/display_tweaks.h
+++ src/setting/display_tweaks.h
@@ -4,7 +4,7 @@ namespace setting {
     class display_tweaks {
     public:
         static void load_setting();
-        
+
         static float get_resolution_scale();
         static bool get_borderless_upscale();
     };
